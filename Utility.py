0aef103715594f9575dc438b56f8def84de76ca5
Michal <michalpl2003@gmail.com>
2021-03-21 20:41:37 +0000

    Cleaning up code + adding plotGraph function

--- Utility.py
+++ Utility.py
@@ -1,12 +1,32 @@
 import matplotlib.pyplot as plt
 
-# Constants
-SiY = 31556952
-
 
 # functions
 def add(a, b):
     return a + b
+
+
+def plotGraph(t, T, Title, x_axis, y_axis, celciusLine):
+    fig = plt.figure()
+    plt.plot(t, T, c='r', linewidth=2.25)
+
+    if celciusLine == 'true':
+        plt.plot([t[0], t[-1]], [273.15, 273.15], c='c', label='0°C', lw='0.7', linestyle='dashed')
+
+        plt.legend(loc="lower right", title='Extra lines:', framealpha=1.0)
+
+    # Adding labels for title and axes
+    fig.suptitle(Title, fontsize=12)
+    plt.xlabel(x_axis, fontsize=9)
+    plt.ylabel(y_axis, fontsize=9)
+    plt.minorticks_on()  # minor ticks
+
+    # Drawing major & minor gridlines
+    plt.grid(b=True, which='major', color='black', linestyle='-', linewidth=0.5)
+    plt.grid(b=True, which='minor', color='grey', linestyle=':', linewidth=0.2)
+
+    # Displaying plot
+    plt.show()
 
 
 def zeroD_EBM():
@@ -14,7 +34,7 @@ def zeroD_EBM():
     waterDepth = 4000  # (m)
     L = 1350  # (W / m^2)
     albedo = 0.3  # how much light gets reflected by atmosphere
-    epsilon = 1  # how good of a blackbody the body is
+    epsilon = 0.77  # how good of a blackbody the body is
     sigma = 5.67E-8  # W/m^2 K4
     heat_capacity = waterDepth * 1000 * 4200  # (J / K m^2)
     heat_in = (L * (1 - albedo)) / 4  # Watts/m^2
@@ -32,33 +52,4 @@ def zeroD_EBM():
         heat_content += (heat_in - heat_out) * timeStep * 3.154e7
         T.append(heat_content / heat_capacity)  # (K)
 
-    fig = plt.figure()
-    plt.plot(t, T, c='r', linewidth=2.25)
-    # Plots line for 0* Celsius
-    plt.plot([0, years], [273.15, 273.15], c='c', label='0°C', lw='0.7', linestyle='dashed')
-
-    plt.legend(loc="lower right", title='Extra lines:', framealpha=1.0)
-
-    # Adding labels for title and axes
-    fig.suptitle('0D EBM without Greenhouse effect', fontsize=12)
-    plt.xlabel('time (years)', fontsize=9)
-    plt.ylabel('Surface temperature (K)', fontsize=9)
-    plt.axis([0, 1600, 0, 300])  # Specifying range for axes
-    plt.minorticks_on()  # minor ticks
-
-    # Drawing major & minor gridlines
-    plt.grid(b=True, which='major', color='black', linestyle='-', linewidth=0.5)
-    plt.grid(b=True, which='minor', color='grey', linestyle=':', linewidth=0.2)
-
-    # Saving plot locally
-    iName = '0D EBM without GHE'
-    dpi = 500
-    print('Saving to ' + iName + '.png')
-    fig.savefig('C:\\Users\\Michal\\Desktop\\Remote Lessons\\CREST\\dump\\' + iName + '.png', dpi=dpi)
-    print('Plot saved to ' + iName + '.png')
-    print('Saving to ' + iName + '_2x.png')
-    fig.savefig('C:\\Users\\Michal\\Desktop\\Remote Lessons\\CREST\\dump\\' + iName + '_2x.png', dpi=(2 * dpi))
-    print('Plot saved to ' + iName + '_2x.png')
-
-    # Displaying plot
-    plt.show()
+    return t, T
--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from Utility import *
 
 print("Which model would you like to see?")
-print("A - OD EBM")
-print("B - OD EBM w/ eccentricity variation")
-print("C - OD EBM w/ eccentricity variation - Bar")
-print("D - OD EBM w/ eccentricity variation - Min/Max/Mean")
-print("E - OD EBM w/ Greenhouse effect")
-print("F - 1D EBM")
-print("g - 1D EBM w/ Greenhouse effect")
+print("0 - OD EBM")
+print("1 - OD EBM w/ eccentricity variation")
+print("2 - OD EBM w/ eccentricity variation - Bar")
+print("3 - OD EBM w/ eccentricity variation - Min/Max/Mean")
+print("4 - OD EBM w/ Greenhouse effect")
+print("5 - 1D EBM")
+print("6 - 1D EBM w/ Greenhouse effect")
+print("7 - Solar insolation on Eccentric orbit")
+print("8 - Solar insolation on different eccentric orbits")
+print("9 - Latitude stepping 1D EBM w/ Greenhouse Effect")
+selection = int(input())
 
-zeroD_EBM()
+if selection == 0:
+    print("0D EBM")
+    t, T = zeroD_EBM()
+    plotGraph(t, T, '0D EBM without Greenhouse effect', 'time (years)', 'Surface temperature (K)', 'true')
+
+elif selection == 1:
+    print("OD EBM w/ eccentricity variation")
+elif selection == 2:
+    print("OD EBM w/ eccentricity variation")
+elif selection == 3:
+    print("OD EBM w/ eccentricity variation")
+elif selection == 4:
+    print("OD EBM w/ eccentricity variation")
+elif selection == 5:
+    print("OD EBM w/ eccentricity variation")
+elif selection == 6:
+    print("OD EBM w/ eccentricity variation")
+elif selection == 7:
+    print("OD EBM w/ eccentricity variation")
+elif selection == 8:
+    print("OD EBM w/ eccentricity variation")
+elif selection == 9:
+    print("OD EBM w/ eccentricity variation")

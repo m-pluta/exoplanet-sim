de22f09708924d483b1ad55c904762306dbbf4af
Michal <michalpl2003@gmail.com>
2021-04-14 19:38:12 +0100

    Implemented all methods into zeroD_e_mmm.py

--- models/zeroD_e_mmm.py
+++ models/zeroD_e_mmm.py
@@ -2,81 +2,66 @@ import math
 
 import matplotlib.pyplot as plt
 
+import c
 from Utility import beautifyPlot
-from Utility import distance_of_planet_to_star
+from Utility import generateEccentricityList
+from Utility import generate_heat_in
 from Utility import plotCelciusLine
+from Utility import solarOutput
 
 
 def zeroD_e_mmm(plotTitle):
     waterDepth = 400  # (m)
-    albedo = 0.3  # how much light gets reflected by atmosphere
-    epsilon = 1  # how good of a blackbody the body is
-    sigma = 5.67E-8  # W/m^2 K4
-    heat_capacity = waterDepth * 1000 * 4200  # (J / K m^2)
-    SiY = 31557600  # Seconds in a year
+    albedo = c.albedo_Earth  # how much light gets reflected by atmosphere
+    epsilon = c.epsilon_Earth  # how good of a blackbody the body is
 
-    R_star = 0.00465047  # Radius of star (AU)
-    d_planet = 1  # Distance of planet from body it is orbiting  (AU)
+    R_star = c.R_Sun  # Radius of star (AU)
+    d_planet = c.d_Earth  # Distance of planet from body it is orbiting  (AU)
     T_star = 5778  # Surface Temperature of star (K)
-    e = 0  # Eccentricity of planet
-    Period_Fractions = 1000  # number of fractions of period
+    periodFractions = 1000  # number of fractions of period
 
-    # Init
+    # Global Initialisation
+    heat_capacity = waterDepth * 1000 * 4200  # (J / K m^2)
     period = math.pow(d_planet, 1.5)  # Period of planet's orbit (years)
-    Power_output = (4 * math.pi * R_star ** 2 * sigma * T_star ** 4) / (
-            4 * math.pi * d_planet ** 2)  # Power output of star (Watts)
+    Power_output = solarOutput(R_star, T_star, d_planet)  # incidentPower from star (W)
 
+    eccentricities = generateEccentricityList(0.5, 0.99, 0.01)
     mins = []
     means = []
     maxs = []
-    eccentricities = []
 
-    for j in range(1, 100):  # Iterating through each eccentricity from 0.01 to 0.99
+    for e in eccentricities:  # Iterating through each eccentricity from 0.01 to 0.99
+
+        # Initialisation
         T = [0]
-        heat_in = []
-
-        e += 0.01
-        eccentricities.append(e)
-
-        # Generating Heat_in coefficients
-        for i in range(1, Period_Fractions + 1):
-            theta = (i / Period_Fractions) * 2 * math.pi  # Calculating angle in orbit
-            r = distance_of_planet_to_star(theta, d_planet, e)  # Applying Kepler's First Law to find r
-            L = Power_output / (
-                    r / d_planet) ** 2  # Calculating insolation based on distance from star relative to semi major axis
-            heat_in.append((L * (1 - albedo)) / 4)
-
-        # Generating Data
         heat_content = heat_capacity * T[0]  # (J / m^2)
         years = 250
-        steps = int(years / (period / Period_Fractions))
+        steps = int(years / (period / periodFractions))
+        tempMin = 1E24
+        heat_in = generate_heat_in(e, periodFractions, d_planet, Power_output, albedo)
 
-        tempMin = 10000
         tempMax = 0
         tempMean = 0
         countMean = 0
 
         for i in range(steps):
-            heat_out = epsilon * sigma * pow(T[-1], 4)
+            heat_out = epsilon * c.sigma * pow(T[-1], 4)
 
-            heat_content += (heat_in[i % Period_Fractions] - heat_out) * period / Period_Fractions * SiY
+            heat_content += (heat_in[i % periodFractions] - heat_out) * period / periodFractions * c.SiY
             T.append(heat_content / heat_capacity)  # (K)
-            # print(t[-1], T[-1])  # For Debugging
-            if i > 240 / (period / Period_Fractions):
-                if T[-1] < tempMin:
-                    tempMin = T[-1]  # keeping track of the max and min temperatures for each eccentricity
 
-                if T[-1] > tempMax:
-                    tempMax = T[-1]
+            if i > (years - 10 * period) / (period / periodFractions):  # Only keeps track of minimum temperatures for the last 10 periods in EBM
+                if T[-1] < tempMin:
+                    tempMin = T[-1]  # keeping track of the minimum temperature
 
                 tempMean += T[-1]
                 countMean += 1
 
         mins.append(tempMin)
-        maxs.append(tempMax)
+        maxs.append(max(T))
         means.append(tempMean / countMean)
 
-        print(str(round(e, 2)))
+        print(str(round(e, 2) * 100))
 
     # Plotting data
     fig = plt.figure(plotTitle)

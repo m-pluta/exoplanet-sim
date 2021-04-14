13f9ca277bf17556450451c99594b3b29dfaebe1
Michal <michalpl2003@gmail.com>
2021-04-14 03:57:46 +0100

    put constants in a seperate file

---
+++ c.py
@@ -0,0 +1,2 @@
+sigma = 5.67E-8  # W/m^2 K4
+SecondsInYear = 31556952  # seconds (s)
--- models/zeroD.py
+++ models/zeroD.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+import c
 from Utility import beautifyPlot
 from Utility import plotCelciusLine
 
@@ -12,10 +13,6 @@ def zeroD_EBM():
     albedo = 0.3  # how much light gets reflected by atmosphere
     epsilon = 0.77  # how good of a blackbody the body is
 
-    # Constants
-    sigma = 5.67E-8  # W/m^2 K4
-    SecondsInYear = 31556952
-
     # Init
     heat_capacity = waterDepth * 1000 * 4200  # (J / K m^2)
     heat_in = (L * (1 - albedo)) / 4  # Watts/m^2
@@ -26,9 +23,9 @@ def zeroD_EBM():
     heat_content = heat_capacity * T[0]  # (J / m^2)
     years = int(input('Number of years (1500): '))
     for i in range(int(years / timeStep)):
-        heat_out = epsilon * sigma * pow(T[-1], 4)
+        heat_out = epsilon * c.sigma * pow(T[-1], 4)
         t.append(t[-1] + timeStep)
-        heat_content += (heat_in - heat_out) * timeStep * SecondsInYear
+        heat_content += (heat_in - heat_out) * timeStep * c.SecondsInYear
         T.append(heat_content / heat_capacity)  # (K)
 
     # Plotting data

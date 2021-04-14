3642c4a42197319d10d463b897fa0e227947c4c6
Michal <michalpl2003@gmail.com>
2021-04-14 22:18:14 +0100

    Store e and color as list of tuples

--- models/insolation_multiple.py
+++ models/insolation_multiple.py
@@ -3,15 +3,14 @@ from models.insolation_single import *
 
 
 def insolation_multiple(plotTitle):
-    eccentricities = [0, 0.2, 0.4, 0.7]
-    colors = ['r', 'g', 'b', 'y']
+    eccentricities = [(0, 'c'), (0.2, 'g'), (0.4, 'b'), (0.6, 'y'), (0.8, 'm')]
 
     # Plotting data
     fig = plt.figure(plotTitle)
 
-    for i in range(0, 4):
-        t, L = insolation_single('', True, eccentricities[i])
-        plt.plot(t, L, c=colors[i], lw=1.25, label=('e = ' + str(eccentricities[i])))
+    for i in range(0, len(eccentricities)):
+        t, L = insolation_single('', True, eccentricities[i][0])
+        plt.plot(t, L, c=eccentricities[i][1], lw=1.25, label=('e = ' + str(eccentricities[i][0])))
 
     # Modifying Visual aspect of plot
     fig = beautifyPlot(fig, plotTitle, 'time (days)', 'Light Insolation (W/m^2)')

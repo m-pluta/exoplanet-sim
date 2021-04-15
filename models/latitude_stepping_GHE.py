7375542d6583864b88676fce9902aae571dec1c2
Michal <michalpl2003@gmail.com>
2021-04-15 01:04:14 +0100

    added smooth albedo f(x), epsilonS=1 results in no interesting results.

--- models/latitude_stepping_GHE.py
+++ models/latitude_stepping_GHE.py
@@ -2,23 +2,27 @@ import math
 
 import matplotlib.pyplot as plt
 
-from Utility import beautifyPlot, plotCelciusLine, addLegend
+import c
+from Utility import beautifyPlot, plotCelciusLine, addLegend, solarConstant
 
 
 def latitude_stepping_GHE(plotTitle):
-    # Constants
+    # Independent Variables
+    R_star = c.R_Sun  # Radius of star (AU)
+    d_planet = c.d_Earth  # Distance of planet from body it is orbiting  (AU)
+    T_star = c.T_Sun  # Surface Temperature of star (K)
+    albedo = c.albedo_Earth
+    epsilonS = 0.7
+    epsilonA = c.epsilonA_Earth
     timeStep = 1  # years
     waterDepth = 4000  # m
-    L = 1361  # W/m^2
-    albedo = 0.3
-    epsilonS = 0.7
-    epsilonA = 0.77
-    sigma = 5.67E-8  # W/m^2/K^4
-    heatCapacity = waterDepth * 4.2E6  # J/K/m^2
     latitudeWidth = 1  # degrees
-    SiY = 31536000  # Seconds in a year
 
     fac = 2  # the factor used to allow <latitudeWidth/fac> increments in latitude
+
+    # Initialisation
+    heatCapacity = waterDepth * 4.2E6  # J/K/m^2
+    L = solarConstant(R_star, T_star, d_planet)  # W/m^2
     t = [0]
     latitudes = []
 
@@ -26,33 +30,26 @@ def latitude_stepping_GHE(plotTitle):
     T = []
 
     for i in range(-90 * fac, 90 * fac, latitudeWidth):
-        # Ratio of height of arc (from the view of a cross section of the earth) to the length of the arc
-        # i.e Ratio of Flux in vs Flux out
-        ratio = (math.sin(math.radians((i + latitudeWidth) / fac)) - math.sin(math.radians(i / fac))) / (
-                ((latitudeWidth / fac) / 360) * 2 * math.pi)
+        # Ratio of height of arc (from the view of a cross section of the earth) to the length of the arc i.e Ratio of Flux in vs Flux out
+        ratio = (math.sin(math.radians((i + latitudeWidth) / fac)) - math.sin(math.radians(i / fac))) / (((latitudeWidth / fac) / 360) * 2 * math.pi)
         # Creates a dictionary element for each latitude
         latitudes.append(
             {'lat': (i / fac, (i + latitudeWidth) / fac), 'tempList': [0.0], 'heatContent': 0, 'albedo': albedo,
              'ratio': ratio})
         l.append(i / fac)
 
-    years = 1000000  # Arbitrary value
-    iceAlbedoThreshold = 273.15 - 40  # Minimum temp for ice properties to start changing
+    years = 1000000  # Arbitrary value - the value of this doesnt actually matter too much
+    iceAlbedoThreshold = 223.15  # Minimum temp for ice properties to start changing
 
     for lat in latitudes:
-        for i in range(1, int(years / timeStep)):
-            if lat['tempList'][-1] < iceAlbedoThreshold:
-                lat['albedo'] = 0.7
-            elif iceAlbedoThreshold <= lat['tempList'][-1] <= 273.15:
-                # ensures albedo transitions smoothly between 0.7 and 0.3 depending on temperature
-                lat['albedo'] = 0.7 - 0.4 * ((lat['tempList'][-1] - iceAlbedoThreshold) / (273.15 - iceAlbedoThreshold))
-            else:
-                lat['albedo'] = albedo
+        for i in range(int(years / timeStep)):
+            lat['albedo'] = smoothAlbedo(lat['tempList'][-1], iceAlbedoThreshold, 273.15, albedo, 0.7)  # Linear interpolation
+
             tempA = (0.5 * epsilonS * lat['tempList'][-1] ** 4) ** 0.25  # Temp of atmosphere assuming energy balance
             FluxIn = (L * (1 - lat['albedo'])) / 4 * lat['ratio']  # W/m^2
-            FluxOut = (1 - epsilonA) * epsilonS * sigma * (lat['tempList'][-1] ** 4) + epsilonA * sigma * tempA ** 4
+            FluxOut = (1 - epsilonA) * epsilonS * c.sigma * (lat['tempList'][-1] ** 4) + epsilonA * c.sigma * tempA ** 4
             FluxNet = FluxIn - FluxOut
-            lat['heatContent'] += FluxNet * SiY * timeStep
+            lat['heatContent'] += FluxNet * c.SiY * timeStep
             lat['tempList'].append(lat['heatContent'] / heatCapacity)
             if len(lat['tempList']) > 2 and lat['tempList'][-2] != 0:
                 if (lat['tempList'][-1] - lat['tempList'][-2]) / lat['tempList'][-2] < 1E-17:
@@ -71,3 +68,13 @@ def latitude_stepping_GHE(plotTitle):
     fig = addLegend(fig)
 
     return fig
+
+
+def smoothAlbedo(currentTemp, iceAlbedoThreshold=263.15, MinAlbedoTemperature=273.15, minAlbedo=0.3, maxAlbedo=0.7):
+    if currentTemp < iceAlbedoThreshold:
+        return maxAlbedo
+    elif iceAlbedoThreshold <= currentTemp <= MinAlbedoTemperature:
+        # ensures albedo transitions smoothly between 0.7 and 0.3 depending on temperature - this is basically linear interpolation
+        return maxAlbedo - (maxAlbedo - minAlbedo) * (currentTemp - iceAlbedoThreshold) / (MinAlbedoTemperature - iceAlbedoThreshold)
+    else:
+        return minAlbedo

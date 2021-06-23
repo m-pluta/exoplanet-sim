12583952e93124498f608ab059c566e262a998f0
Michal <michalpl2003@gmail.com>
2021-06-24 00:25:16 +0100

    Seperating Insolation calculations into seperate components to make it easier to modify and vary later. Instead of using a timestep, i've updated the model to split the period into fractions just like the other models Nested the for loop for it loops through the specified amount of periods.

--- models/zeroD.py
+++ models/zeroD.py
@@ -1,34 +1,34 @@
-import matplotlib.pyplot as plt
-
-import c
-from Utility import addLegend
-from Utility import beautifyPlot
-from Utility import plotCelciusLine
-from Utility import solarConstant
+from Utility import *
 
 
 def zeroD_EBM(plotTitle):
-    # Independent variables
-    timeStep = 0.2  # (y)
+    # Independent Variables
     waterDepth = 4000  # (m)
-    L = solarConstant(c.T_Sun, c.R_Sun, c.d_Earth)
     albedo = c.albedo_Earth  # how much light gets reflected by atmosphere
-    epsilon = c.epsilonA_Earth  # how good of a blackbody the body is
+    epsilon = c.epsilonS_Earth  # how good of a blackbody the body is
+    R_star = c.R_Sun  # Radius of star (AU)
+    d_planet = c.d_Earth * 4  # Distance of planet from body it is orbiting  (AU)
+    T_star = c.T_Sun  # Surface Temperature of star (K)
+    periodFractions = 1000  # number of fractions of period
 
     # Initialisation
     heat_capacity = waterDepth * 1000 * 4200  # (J / K m^2)
-    heat_in = (L * (1 - albedo)) / 4  # Watts/m^2
+    period = math.pow(d_planet, 1.5)  # Period of planet's orbit (years)
+    Power_output = PowerOutput(T_star)  # Power output of star (Watts/m^2)
+    solar_Constant = planetInsolation(Power_output, R_star, d_planet)
+    heat_in = (solar_Constant * (1 - albedo)) / 4  # Watts/m^2
     t = [0]
     T = [0]
 
     # Generating Data
     heat_content = heat_capacity * T[0]  # (J / m^2)
-    years = int(input('Number of years (1500): '))
-    for i in range(int(years / timeStep)):
-        heat_out = epsilon * c.sigma * pow(T[-1], 4)
-        t.append(t[-1] + timeStep)
-        heat_content += (heat_in - heat_out) * timeStep * c.SiY
-        T.append(heat_content / heat_capacity)  # (K)
+    periods = int(input('Number of periods (1000): '))
+    for i in range(periods):
+        for j in range(periodFractions):
+            t.append(t[-1] + period / periodFractions)
+            heat_out = epsilon * c.sigma * T[-1] ** 4
+            heat_content += (heat_in - heat_out) * (period / periodFractions) * c.SiY
+            T.append(heat_content / heat_capacity)  # (K)
 
     # Plotting data
     fig = plt.figure(plotTitle)

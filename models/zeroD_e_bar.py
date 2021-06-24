f9cd2378ffece69141678414d07e0366ec0ab649
Michal <michalpl2003@gmail.com>
2021-06-24 01:17:33 +0100

    Simplified import list More accurate descriptions of variables Seperated Power output and insolation so it is easier to vary later on Nested the data-generating for loop with the amount of periods as the iterator Adjusted minimum height of bars to 2 as for eccentricities < 0.2 for waterDepth = 800 the bars are all the same size which is misleading and harder to debug More descriptive variable names for lists containing minimum temperatures and maximum temperatures

--- models/zeroD_e_bar.py
+++ models/zeroD_e_bar.py
@@ -1,72 +1,61 @@
-import math
-
-import matplotlib.pyplot as plt
 from PyAstronomy import pyasl
 
-import c
-from Utility import addLegend, generate_heat_in
-from Utility import beautifyPlot
-from Utility import generateEccentricityList
-from Utility import plotCelciusLine
-from Utility import solarConstant
+from Utility import *
 
 
 def zeroD_e_bar(plotTitle):
     # Independent Variables
-    waterDepth = 400  # (m)
-    albedo = c.albedo_Earth  # how much light gets reflected by atmosphere
-    epsilon = c.epsilonA_Earth  # how good of a blackbody the body is
-
+    waterDepth = 800  # (m)
+    albedo = c.albedo_Earth  # how much light gets reflected by atmosphere (0-1)
+    epsilon = c.epsilonSurface_Earth  # how good of a blackbody the body is (0-1)
     R_star = c.R_Sun  # Radius of star (AU)
     d_planet = c.d_Earth  # Distance of planet from body it is orbiting  (AU)
-    T_star = 5778  # Surface Temperature of star (K)
-    periodFractions = 1000  # number of fractions of period
+    T_star = c.T_Sun  # Surface Temperature of star (K)
+    periodFractions = 10  # number of fractions of period
 
     # Global Initialisation
     heat_capacity = waterDepth * 1000 * 4200  # (J / K m^2)
     period = math.pow(d_planet, 1.5)  # Period of planet's orbit (years)
-    Power_output = solarConstant(T_star, R_star, d_planet)  # incidentPower from star (W)
-
-    eccentricities = generateEccentricityList(0, 0.99, 0.01)
-    mins = []
-    maxs = []
+    Power_Output = PowerOutput(T_star)  # Power irradiated from the star's surface (W/m^2)
+    solar_Constant = planetInsolation(Power_Output, R_star, d_planet)  # Insolation incident on the planet's surface (W/m^2)
+    eccentricities = generateEccentricityList(0, 1, 0.01)  # List containing all the eccentricities being plotted
+    minTemps = []
+    maxTemps = []
 
     for e in eccentricities:  # Iterating through each eccentricity from 0.01 to 0.99
 
         # Initialisation
         T = [0]
         heat_content = heat_capacity * T[0]  # (J / m^2)
-        years = 250
-        steps = int(years / (period / periodFractions))
+        periods = 250
         tempMin = 1E24
         ke = pyasl.KeplerEllipse(d_planet, period, e, Omega=0., i=0.0, w=0.0)
-        heat_in = generate_heat_in(ke, periodFractions, d_planet, Power_output, albedo)
+        heat_in = generate_heat_in(ke, periodFractions, d_planet, solar_Constant, albedo)
+        for k in range(periods):
+            for i in range(periodFractions):
+                heat_out = epsilon * c.sigma * pow(T[-1], 4)
 
-        for i in range(steps):
-            heat_out = epsilon * c.sigma * pow(T[-1], 4)
+                heat_content += (heat_in[i] - heat_out) * (period / periodFractions * c.SiY)
+                T.append(heat_content / heat_capacity)  # (K)
+                if periods - k < 20:  # Only keeps track of minimum temperatures for the last 20 periods in EBM
+                    if T[-1] < tempMin:
+                        tempMin = T[-1]  # Keeping track of the minimum temperature
 
-            heat_content += (heat_in[i % periodFractions] - heat_out) * period / periodFractions * c.SiY
-            T.append(heat_content / heat_capacity)  # (K)
-            if i > (years - 10 * period) / (period / periodFractions):  # Only keeps track of minimum temperatures for the last 10 periods in EBM
-                if T[-1] < tempMin:
-                    tempMin = T[-1]  # keeping track of the minimum temperature
-
-        mins.append(tempMin)
-        maxs.append(max(T))
+        minTemps.append(tempMin)
+        maxTemps.append(max(T))
 
     # Plotting data
     fig = plt.figure(plotTitle)
 
-    tempE = 0
-    for i in range(0, len(eccentricities)):
+    for i in range(len(eccentricities)):
         # Height minimum
-        minBarHeight = 5
-        height = round(maxs[i] - mins[i], 3)
+        minBarHeight = 2
+        height = round(maxTemps[i] - minTemps[i], 3)
         if height < minBarHeight:
             height = minBarHeight
 
         barWidth = 0.006  # Set to 0.006 for default or 0.01 for connected bars
-        plt.bar(round(eccentricities[i], 2), height, width=barWidth, bottom=round(mins[i], 3), align='center', color='r')
+        plt.bar(round(eccentricities[i], 2), height, width=barWidth, bottom=round(minTemps[i], 3), align='center', color='r')
 
     # Modifying Visual aspect of plot
     fig = beautifyPlot(fig, plotTitle, 'Eccentricity', 'Surface temperature (K)')

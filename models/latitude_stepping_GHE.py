c8844941d73bd5b6cbf3ec3b65fac3f0166ae8c8
Michal <michalpl2003@gmail.com>
2021-06-28 22:48:49 +0100

    Made the factor variable redundant by implementing a more easy to understand method of generating all the different iterable latitudes using the generateList() function. Implemented a more accurate 'ratio' approximation for heat_in/heat_out formula => this is still not a good aproximation however it is good enough for now.

--- models/latitude_stepping_GHE.py
+++ models/latitude_stepping_GHE.py
@@ -16,33 +16,32 @@ def latitude_stepping_GHE(plotTitle):
 
     # Initialisation
     period = math.pow(d_planet, 1.5)  # Period of planet's orbit (years)
-    heatCapacity = waterDepth * 4.2E6  # J/K/m^2
+    heatCapacity = waterDepth * 1000 * 4200  # J/K/m^2
     L = solarConstant(T_star, R_star, d_planet)  # W/m^2
-    latitudes = []
+    latitudeData = []
 
-    l = []
+    latitudes = generateList(-90, 90, latitudeWidth)
     T = []
 
-    factor = 1  # the factor used to allow <latitudeWidth/fac> increments in latitude
-    magnitude = math.floor(math.log10(latitudeWidth))
-    if magnitude < 0:  # Finds magnitude of latitudeWidth and scales for loop parameters so they are all integers depending on the magnitude
-        factor = math.pow(10, abs(magnitude))
+    # factor = 1  # the factor used to allow <latitudeWidth/fac> increments in latitude
+    # magnitude = math.floor(math.log10(latitudeWidth))
+    # if magnitude < 0:  # Finds magnitude of latitudeWidth and scales for loop parameters so they are all integers depending on the magnitude
+    #     factor = math.pow(10, abs(magnitude))
 
-    for i in range(int(-90 * factor), int(90 * factor), int(latitudeWidth * factor)):
+    for l in latitudes:
         # Ratio of height of arc (from the view of a cross section of the earth) to the length of the arc i.e Ratio of Flux in vs Flux out
-        ratio = (math.sin(math.radians((i + latitudeWidth) / factor)) - math.sin(math.radians(i / factor))) / (((latitudeWidth / factor) / 360) * 2 * math.pi)
+        # ratio = math.sin(math.radians(abs(l) + abs(latitudeWidth))) - math.sin(math.radians(abs(l))) / ((latitudeWidth / 360) * 2 * math.pi)
+        ratio = abs(math.sin(math.radians(abs(l + 1))) - math.sin(math.radians(abs(l)))) / ((latitudeWidth / 360) * 2 * math.pi)
         # Creates a dictionary element for each latitude
-        latitudes.append(
-            {'lat': (i / factor, (i + latitudeWidth) / factor), 'tempList': [StartingTemperature], 'heatContent': StartingTemperature * heatCapacity, 'albedo': albedo,
-             'ratio': ratio})
-        l.append(i / factor)
+        latitudeData.append({'lat': (l, (l + latitudeWidth)), 'tempList': [StartingTemperature], 'heatContent': StartingTemperature * heatCapacity, 'albedo': albedo, 'ratio': ratio})
+        print(latitudeData[-1])
 
-    periods = 10000  # Arbitrary value - the value of this doesnt actually matter too much
+    periods = 10000  # Arbitrary value
 
-    for lat in latitudes:
+    for lat in latitudeData:
         addedTemp = False
         for i in range(periods * periodFractions):
-            lat['albedo'] = smoothAlbedo_linear(lat['tempList'][-1])  # Linear interpolation
+            lat['albedo'] = smoothAlbedo_quadratic(lat['tempList'][-1])  # Linear interpolation
 
             temp_atmosphere = (0.5 * epsilonS * lat['tempList'][-1] ** 4) ** 0.25  # Temp of atmosphere assuming energy balance
             heat_in = (L * (1 - lat['albedo'])) / 4 * lat['ratio']  # W/m^2
@@ -61,11 +60,11 @@ def latitude_stepping_GHE(plotTitle):
 
     # Plotting data
     fig = plt.figure(plotTitle)
-    plt.plot(l, T, c='r', linewidth=1.75)
+    plt.plot(latitudes, T, c='r', linewidth=1.75)
 
     # Modifying Visual aspect of plot
     fig = beautifyPlot(fig, plotTitle, 'Latitude (°)', 'Stable Surface Temperature (K)')
-    fig = plotCelciusLine(fig, l[0], l[-1])
+    fig = plotCelciusLine(fig, latitudes[0], latitudes[-1])
     fig = addLegend(fig)
 
     return fig

ad587c3ae6a242f390ee95432932e6c41f1e04de
Michal <michalpl2003@gmail.com>
2021-06-28 20:04:09 +0100

    Modifying both oneD model to fit the new spec

--- models/oneD.py
+++ models/oneD.py
@@ -1,25 +1,21 @@
-import math
-
-import matplotlib.pyplot as plt
-
-import c
-from Utility import beautifyPlot, addLegend, plotCelciusLine, solarConstant
+from Utility import *
 
 
 def oneD(plotTitle):
     # Independent Variables
-    timeStep = 0.1  # years
     waterDepth = 4000  # m
     R_star = c.R_Sun  # Radius of star (AU)
     d_planet = c.d_Earth  # Distance of planet from body it is orbiting  (AU)
     T_star = c.T_Sun  # Surface Temperature of star (K)
     albedo = c.albedo_Earth
     epsilon = c.epsilonSurface_Earth
+    periodFractions = 100
     latitudeWidth = 10  # degrees
 
     # Initialisation
+    period = math.pow(d_planet, 1.5)  # Period of planet's orbit (years)
+    heatCapacity = waterDepth * 1000 * 4200  # J/K/m^2
     L = solarConstant(T_star, R_star, d_planet)  # W/m^2
-    heatCapacity = waterDepth * 4.2E6  # J/K/m^2
     t = [0]
     latitudes = []
 
@@ -30,19 +26,21 @@ def oneD(plotTitle):
         # Creates a dictionary element for each latitude
         latitudes.append({'lat': (i, i + latitudeWidth), 'tempList': [0.0], 'heatContent': 0, 'albedo': albedo, 'ratio': ratio})
 
-    years = int(input('Number of years (24000): '))  # Arbitrary value
-    for i in range(1, int(years / timeStep)):  # For each time step in the given amount of year
-        for lat in latitudes:  # Goes through each latitude
-            if lat['tempList'][-1] < 273.15:  # If colder than 0*C then higher albedo due to snow cover
-                lat['albedo'] = 0.7
-            else:
-                lat['albedo'] = albedo  # If warmer than 0*C then lower albedo due to snow melting
-            heat_in = (L * (1 - lat['albedo'])) / 4 * lat['ratio']  # W/m^2
-            heat_out = (epsilon * c.sigma * lat['tempList'][-1] ** 4)  # W/m^2
-            net_heat = heat_in - heat_out
-            lat['heatContent'] += net_heat * c.SiY * timeStep
-            lat['tempList'].append(lat['heatContent'] / heatCapacity)
-        t.append(t[-1] + timeStep)
+    periods = int(input('Number of periods (15000): '))  # Arbitrary value
+    for k in range(periods):
+        for i in range(periodFractions):  # For each time step in the given amount of year
+            for lat in latitudes:  # Goes through each latitude
+                if lat['tempList'][-1] < 273.15:  # If colder than 0*C then higher albedo due to snow cover
+                    lat['albedo'] = 0.7
+                else:
+                    lat['albedo'] = albedo  # If warmer than 0*C then lower albedo due to snow melting
+                heat_in = (L * (1 - lat['albedo'])) / 4 * lat['ratio']  # W/m^2
+                heat_out = (epsilon * PowerOut(lat['tempList'][-1]))  # W/m^2
+                net_heat = heat_in - heat_out
+                lat['heatContent'] += net_heat * (period / periodFractions) * c.SiY
+                lat['tempList'].append(lat['heatContent'] / heatCapacity)
+            t.append(t[-1] + (period / periodFractions))
+        print(k)
 
     # Plotting data
     fig = plt.figure(plotTitle)
@@ -52,7 +50,7 @@ def oneD(plotTitle):
 
     # Modifying Visual aspect of plot
     fig = beautifyPlot(fig, plotTitle, 'time (years)', 'Surface temperature (K)')
-    fig = plotCelciusLine(fig, 0, years)
+    fig = plotCelciusLine(fig, 0, t[-1])
     fig = addLegend(fig, title='Latitudes: ')
 
     return fig
